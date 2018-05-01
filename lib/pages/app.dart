231ce92421a0699db18387a50cd4f1384e55a584
creatint <creatint@163.com>
2018-05-01 14:41:30 +0800

    Add route to ImportLocal

--- lib/pages/app.dart
+++ lib/pages/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
+import 'import_local.dart';
 
 class App extends StatefulWidget {
   @override
@@ -7,10 +8,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Home(),
+      routes: <String, WidgetBuilder> {
+        'importLocal': (BuildContext context) => new ImportLocal()
+      },
     );
   }
 }
