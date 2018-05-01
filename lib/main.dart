9a95252297cc7fc885746059b776c531240496ff
creatint <creatint@163.com>
2018-05-01 15:09:07 +0800

    Add initial function.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'pages/app.dart';
 import 'services/system.dart';
+import 'utils/initial.dart';
 
 void main() async {
+  /// get instance of SharedPreferences
   SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  /// get instance of SystemService
   new SystemService(prefs: prefs);
+
+  /// initialize the app
+  await initial();
+
+  /// run the app
   runApp(new App());
 }
---
+++ lib/utils/initial.dart
@@ -0,0 +1,5 @@
+import 'dart:async';
+
+Future<Null> initial () async {
+
+}
