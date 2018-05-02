234003118615caa1ad161d60204e9e4d882c6ff6
creatint <creatint@163.com>
2018-05-02 11:54:18 +0800

    Add getFileName, getFileSuffix and getFileType.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,6 +1,14 @@
 import 'dart:io';
 
+enum FileType { TEXT, EPUB, PDF, OTHER, NOT_FOUND, DIRECTORY }
+
+
 RegExp _regBasename = new RegExp(r'[^/\\]+$');
+RegExp _regFileType = new RegExp(r'([^.\\/]+)$');
+RegExp _regTXT = new RegExp(r'txt');
+RegExp _regPDF = new RegExp(r'pdf');
+RegExp _regEPUB = new RegExp(r'epub');
+RegExp _regName = new RegExp(r'(.+)[^.]+$');
 
 String getFileName(dynamic file) {
   if (file is String) {
@@ -10,4 +18,31 @@ String getFileName(dynamic file) {
   } else {
     return '';
   }
+}
+
+String getFileSuffix(dynamic file) {
+  String name = getFileName(file);
+  return _regFileType.firstMatch(name)?.group(1);
+}
+
+FileType getFileType(FileSystemEntity file) {
+  if (file.existsSync()) {
+    if (FileSystemEntity.isDirectorySync(file.path)) {
+      return FileType.DIRECTORY;
+    }
+    FileType type;
+    String suffix = getFileSuffix(file);
+    if (null == suffix || suffix.isEmpty) {
+      type = FileType.OTHER;
+    } else if (_regTXT.hasMatch(suffix))
+      type = FileType.TEXT;
+    else if (_regPDF.hasMatch(suffix))
+      type = FileType.PDF;
+    else if (_regEPUB.hasMatch(suffix))
+      type = FileType.EPUB;
+    else
+      type = FileType.OTHER;
+    return type;
+  }
+  return FileType.NOT_FOUND;
 }
