6ed1706e3e4b0ffa28739e28516ba9196011b0c9
creatint <creatint@163.com>
2018-05-02 15:09:25 +0800

    Add file types and fileIsBook, typeIsBook function.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,14 +1,28 @@
 import 'dart:io';
 
-enum FileType { TEXT, EPUB, PDF, OTHER, NOT_FOUND, DIRECTORY }
-
+enum FileType {
+  TEXT,
+  EPUB,
+  PDF,
+  IMAGE,
+  VIDEO,
+  AUDIO,
+  OTHER,
+  NOT_FOUND,
+  DIRECTORY
+}
 
 RegExp _regBasename = new RegExp(r'[^/\\]+$');
 RegExp _regFileType = new RegExp(r'([^.\\/]+)$');
 RegExp _regTXT = new RegExp(r'txt');
 RegExp _regPDF = new RegExp(r'pdf');
 RegExp _regEPUB = new RegExp(r'epub');
-RegExp _regName = new RegExp(r'(.+)[^.]+$');
+RegExp _regIMAGE = new RegExp(r'jpe?g|png|gif|bmp');
+RegExp _regVIDEO = new RegExp(r'mp4|rmvb|avi|mov|wmv|rm|flash|mid|3gp|'
+    r'mpeg|m4v|mkv|flv|vob|asf|mpeg4');
+RegExp _regAUDIO = new RegExp(r'mp3|ogg|cd|mp3pro|real|wma'
+    r'|ape|midi|vqf');
+//RegExp _regName = new RegExp(r'(.+)[^.]+$');
 
 String getFileName(dynamic file) {
   if (file is String) {
@@ -40,9 +54,34 @@ FileType getFileType(FileSystemEntity file) {
       type = FileType.PDF;
     else if (_regEPUB.hasMatch(suffix))
       type = FileType.EPUB;
+    else if (_regIMAGE.hasMatch(suffix))
+      type = FileType.IMAGE;
+    else if (_regVIDEO.hasMatch(suffix))
+      type = FileType.VIDEO;
+    else if (_regAUDIO.hasMatch(suffix))
+      type = FileType.AUDIO;
     else
       type = FileType.OTHER;
     return type;
   }
   return FileType.NOT_FOUND;
 }
+
+bool typeIsBook (FileType type) {
+  switch (type) {
+    case FileType.TEXT:
+    case FileType.EPUB:
+//    case FileType.PDF:
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool fileIsBook(FileSystemEntity file) {
+  if (FileSystemEntity.isDirectorySync(file.path)) return false;
+  return typeIsBook(getFileType(file));
+}
+
+
+typedef E ValuePipe<T, E>(T value);
