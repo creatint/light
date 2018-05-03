73994a9329ce3b51bf3698cceda339f6f4dec905
creatint <creatint@163.com>
2018-05-03 09:53:05 +0800

    Add constant books_progress.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,3 +4,6 @@ const app_path = 'Yotaku';
 
 /// the key used to store books in prefs
 const books_key = 'books';
+
+/// the key used to store progress of reading
+const books_progress = 'bookProgress';
