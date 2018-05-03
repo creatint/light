52b64bc1ae06d1492a5354cc72064da87e21e53b
creatint <creatint@163.com>
2018-05-03 09:53:26 +0800

    Remove unused import.

--- lib/widgets/book_item.dart
+++ lib/widgets/book_item.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show required;
 import '../models/book.dart';
-import '../utils/utils.dart';
 
 class BookItem extends StatelessWidget {
   BookItem(
