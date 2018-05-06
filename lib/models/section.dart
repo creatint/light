2e8db130ac300a985064a386dc87e5bdc35f4b83
creatint <creatint@163.com>
2018-05-06 22:43:01 +0800

    Add Section model

---
+++ lib/models/section.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart' show required;
+
+class Section {
+  Section({@required this.title, @required this.content});
+
+  final String title;
+  final String content;
+}
